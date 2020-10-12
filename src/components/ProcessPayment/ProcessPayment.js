d4e8ab89ce5ae1273547cc79faf01fe07637e324
mimnets <mdmonirulislammonir@gmail.com>
2020-10-12 16:24:00 +0600

    stripe added successfully

--- src/components/ProcessPayment/ProcessPayment.js
+++ src/components/ProcessPayment/ProcessPayment.js
@@ -1,31 +1,15 @@
 import React from 'react';
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
-import { CardElement } from '@stripe/react-stripe-js';
+import SimpleCardForm from './SimpleCardForm';
 
-const stripePromise = loadStripe('pk_test_JJ1eMdKN0Hp4UFJ6kWXWO4ix00jtXzq5XG');
+const stripePromise = loadStripe('pk_test_BrL7mUapVM2gPd1iGsmnEO0v');
 
 const ProcessPayment = () => {
     return (
         <div>
             <Elements stripe={stripePromise}>
-
-                <CardElement
-                    options={{
-                        style: {
-                            base: {
-                                fontSize: '16px',
-                                color: '#424770',
-                                '::placeholder': {
-                                    color: '#aab7c4',
-                                },
-                            },
-                            invalid: {
-                                color: '#9e2146',
-                            },
-                        },
-                    }}
-                />
+                <SimpleCardForm></SimpleCardForm>
             </Elements>
         </div>
     );
